6925b142f4cfc32f0a2cb0e8ccae01727e9d4a71
Sebastian Waisbrot <sw@demeterr.com>
2014-07-19 18:46:05 -0700

    Show request list

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,31 +1,80 @@
 $(document).on('click', 'input[type=checkbox]', function(ev) {
-   var obj = $(ev.currentTarget);
-   var checked = obj.is(':checked');
+  var obj = $(ev.currentTarget);
+  var checked = obj.is(':checked');
 
-   obj.addClass('csspinner');
-   var cb = function() { obj.removeClass('csspinner'); };
+  obj.addClass('csspinner');
+  var cb = function() { obj.removeClass('csspinner'); };
 
-   switch (obj.attr('name')) {
-      case 'enabled':
-         if (checked) {
-            proxy.start(cb);
-         } else {
-            proxy.stop(cb);
-         }
-         break;
-   }
+  switch (obj.attr('name')) {
+    case 'proxy.enabled':
+      if (checked) {
+        proxy.start(cb);
+      } else {
+        proxy.stop(cb);
+      }
+      break;
+    case 'recording.enabled':
+      if (checked) {
+        proxy.start_recording(cb);
+      } else {
+        proxy.stop_recording(cb);
+      }
+      break;
+  }
 });
 
 $(document).on('change', 'input[type=text]', function(ev) {
-   var obj = $(ev.currentTarget);
-   var value = obj.val();
+  var obj = $(ev.currentTarget);
+  var value = obj.val();
 
-   obj.addClass('csspinner');
-   var cb = function() { obj.removeClass('csspinner'); };
+  obj.addClass('csspinner');
+  var cb = function() { obj.removeClass('csspinner'); };
 
-   switch (obj.attr('name')) {
-      case 'port':
-         proxy.set_port(parseInt(value, 10), cb);
-         break;
-   }
+  switch (obj.attr('name')) {
+    case 'proxy.port':
+      proxy.set_port(parseInt(value, 10), cb);
+      break;
+  }
+});
+
+var keys = ['url', 'method', 'status', 'duration', 'latency',
+    'start_time', 'end_time'];
+var requestById = {};
+var format_request = function(r) {
+  return {
+      id: r.id,
+      url: r.url,
+      method: r.method,
+      status: r.status,
+      duration: r.response_end ? r.response_end - r.request_start + 'ms' : '',
+      latency: r.response_start ? r.response_start - r.request_end + 'ms' : '',
+      start_time: r.request_start ? new Date(parseInt(r.request_start)).toISOString() : '',
+      end_time: r.response_end ? new Date(parseInt(r.response_end)).toISOString() : ''
+  };
+};
+
+proxy.on('requests:initial', function(data) {
+  var options = {
+      valueNames: keys
+  };
+
+  requestList = new List('requests', options);
+  $.each(data, function(i, r) {
+      requestById[r.id] = r;
+      requestList.add(format_request(r));
+  });
+});
+
+proxy.on('requests:add', function(r) {
+  requestById[r.id] = r;
+  requestList.add(format_request(r));
+});
+
+proxy.on('request:info', function(r) {
+  if (r.id === currentHash) {
+      var currentRequest = requestById[r.id];
+      $('#request-data').html(
+          _.template($('#request-data-template').html(),
+              _.extend({}, r, currentRequest)));
+  }
 });
--- public/js/model.js
+++ public/js/model.js
@@ -1,48 +1,90 @@
-var Proxy = require('xv-proxy');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
-var proxy = {
-   proxy: new Proxy(),
-   port: 8080,
-   domain: '0.0.0.0',
-   running: false,
+var XVProxy = require('xv-proxy');
+var MemoryRecorder = require('xv-proxy/lib/recorder/memory');
 
-   start: function(cb) {
-      var self = this;
-      if (this.running) {
-         this.stop(function() {
-            self.start(cb);
-         });
-      } else {
-         this.proxy.listen(this.port, this.domain, function() {
-            self.running = true;
-            if (cb) {
-               cb();
-            }
-         });
-      }
-   },
-   stop: function(cb) {
-      if (this.running) {
-         var self = this;
-         this.proxy.close(function() {
-            self.running = false;
-            if (cb) {
-               cb();
-            }
-         });
-      }
-   },
-
-   set_port: function(port, cb) {
-      this.port = port;
-      if (this.running) {
-         this.start(cb);
-      }
-   },
-   set_domain: function(domain, cb) {
-      this.domain = domain;
-      if (this.running) {
-         this.start(cb);
-      }
-   }
+var Proxy = function(options) {
+  options = options || {};
+  EventEmitter.call(this);
+  this.proxy = new XVProxy();
+  this.port = options.port || 8080;
+  this.domain = options.domain || '0.0.0.0';
+  this.running = false;
+  this.recording = null;
 };
+util.inherits(Proxy, EventEmitter);
+
+Proxy.prototype.start = function(cb) {
+  var self = this;
+  if (this.running) {
+    this.stop(function() {
+      self.start(cb);
+    });
+  } else {
+    this.proxy.listen(this.port, this.domain, function() {
+      self.running = true;
+      if (cb) {
+        cb();
+      }
+    });
+  }
+};
+Proxy.prototype.stop = function(cb) {
+  if (this.running) {
+    var self = this;
+    this.proxy.close(function() {
+      self.running = false;
+      if (cb) {
+        cb();
+      }
+    });
+  }
+};
+
+Proxy.prototype.set_port = function(port, cb) {
+  this.port = port;
+  if (this.running) {
+    this.start(cb);
+  }
+};
+
+Proxy.prototype.set_domain = function(domain, cb) {
+  this.domain = domain;
+  if (this.running) {
+    this.start(cb);
+  }
+};
+
+Proxy.prototype.start_recording = function(cb) {
+  if (this.recording) {
+    return;
+  }
+  var recorder = new MemoryRecorder({});
+  var self = this;
+  recorder.on('request:end', function(request) {
+    self.emit('requests:add', request);
+  });
+  recorder.list_requests(function(err, requests) {
+    if (err) {
+      throw err;
+      return;
+    }
+    self.emit('requests:initial', requests);
+  });
+  this.recording = [
+    recorder.request.bind(recorder),
+    recorder.response.bind(recorder)
+  ];
+  this.proxy.addRequestPipeFactory(this.recording[0]);
+  this.proxy.addResponsePipeFactory(this.recording[1]);
+  cb();
+};
+
+Proxy.prototype.stop_recording = function(cb) {
+  this.proxy.removeRequestPipeFactory(this.recording[0]);
+  this.proxy.removeResponsePipeFactory(this.recording[1]);
+  this.recording = null;
+  cb();
+};
+var proxy = new Proxy();
